c991169d528c931c16b625227b828bc47cf4e18a
Tanushree289 <107938801+Tanushree289@users.noreply.github.com>
2022-08-26 16:53:32 +0530

    Create each_object.js

---
+++ each_object.js
@@ -0,0 +1,33 @@
+    let student1={
+        name:"Steve",
+        rollno:21,
+        class:"10A"
+    }
+    let student2={
+        name:"Hogan",
+        rollno:34,
+        class:"10A"
+    }
+    let student3={
+        name:"Logan",
+        rollno:45,
+        class:"10B"
+    }
+    let student4={
+        name:"Gohan",
+        rollno:12,
+        class:"10A"
+    }
+    let student5={
+        name:"Logan",
+        rollno:31,
+        class:"10B"
+    }
+    let student6={
+        name:"Mohan",
+        rollno:09,
+        class:"10A"
+    }
+    
+    let outstanding = [student1, student2, student3, student4, student5, student6];
+    console.log(outstanding);
