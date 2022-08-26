ca2d4c5f67695536fcdd5c0c8dd9e6e7e4a12b78
Tanushree289 <107938801+Tanushree289@users.noreply.github.com>
2022-08-26 16:55:20 +0530

    Update consider_senario.js

--- consider_senario.js
+++ consider_senario.js
@@ -1,4 +1,7 @@
-    let totalmarks = 0;                                 
+//let studentRecords = [{ name: 'John', id: 123, marks: 98 }, { name: 'Baba', id: 101, marks: 23 }, { name: 'yaga', id: 200, marks: 45 }, { name: 'Wick', id: 115, marks: 75 }];
+// Ques 1 : We are interested in retrieving only the name of the students and all the names should be in caps.['JOHN', 'BABA', 'YAGA', 'WICK']
+
+let totalmarks = 0;                                 
     for(i=0; i<=3; i++){                                
         totalmarks += studentRecords[i].marks;
     }
