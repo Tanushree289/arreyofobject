7875579e413ac7c1f697eeb24b79e4e20c2333b9
Tanushree289 <107938801+Tanushree289@users.noreply.github.com>
2022-08-26 17:00:24 +0530

    Update print_the_sum.js

--- print_the_sum.js
+++ print_the_sum.js
@@ -1,4 +1,5 @@
-    let sum=0;
+   // Ques 6: This time we are required to print the sum of marks of the students with id > 120.
+let sum=0;
     for(i=0; i<=3; i++){                                
         if (studentRecords[i].id > 120){
             sum += studentRecords[i].marks;
