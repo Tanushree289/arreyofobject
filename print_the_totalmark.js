83e89f438cd94bae89dd7e234a9ba06842cef0d6
Tanushree289 <107938801+Tanushree289@users.noreply.github.com>
2022-08-26 17:01:29 +0530

    Update print_the_totalmark.js

--- print_the_totalmark.js
+++ print_the_totalmark.js
@@ -1,4 +1,6 @@
- let gracemarks = 0;
+// Question 7: This time we are required to print the total marks of the students with marks greater than 50 after // a grace of 15 marks has been added to those students who scored less than 50.
+
+let gracemarks = 0;
     for(i=0; i<=3; i++){                               
         if (studentRecords[i].marks < 50){
             studentRecords[i].marks += 15; 
