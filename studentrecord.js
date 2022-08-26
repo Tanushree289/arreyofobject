bf35b45b3850268b5ba5bd8691b0bad5742fa8ec
Tanushree289 <107938801+Tanushree289@users.noreply.github.com>
2022-08-26 16:43:51 +0530

    Create studentrecord.js

---
+++ studentrecord.js
@@ -0,0 +1,5 @@
+    console.log(studentRecords[0].name.toUpperCase()); 
+    console.log(studentRecords[1].name.toUpperCase());
+    console.log(studentRecords[2].name.toUpperCase());
+    console.log(studentRecords[3].name.toUpperCase());
+  
