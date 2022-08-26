8079f5d3734ba816f32ab43d70c62107fe2a5e3a
Tanushree289 <107938801+Tanushree289@users.noreply.github.com>
2022-08-26 17:05:24 +0530

    Update std_sco_more_than50.js

--- std_sco_more_than50.js
+++ std_sco_more_than50.js
@@ -1,3 +1,5 @@
+/ Ques 7: This time we are required to print the total marks of the students with marks greater than 50 after a grace of 15 marks has been added to those students who scored less than 50
+
 let name = [];
 let name1 = studentRecord.map((a)=>{
 if(a.marks>50){
