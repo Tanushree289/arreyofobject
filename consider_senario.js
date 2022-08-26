62b4a947510bd11fcfd56f8a9dfb6ec3a019d708
Tanushree289 <107938801+Tanushree289@users.noreply.github.com>
2022-08-26 16:47:29 +0530

    Create consider_senario.js

---
+++ consider_senario.js
@@ -0,0 +1,5 @@
+    let totalmarks = 0;                                 
+    for(i=0; i<=3; i++){                                
+        totalmarks += studentRecords[i].marks;
+    }
+    console.log(totalmarks);
