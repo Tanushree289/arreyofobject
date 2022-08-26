388a13ac7e184d38b1d34a36b05bed7b69087d52
Tanushree289 <107938801+Tanushree289@users.noreply.github.com>
2022-08-26 17:04:12 +0530

    Update same_dataset.js

--- same_dataset.js
+++ same_dataset.js
@@ -1,4 +1,6 @@
-   for(i=0; i<=3; i++){                               
+ // que2::Supposed we have the same dataset as above but this time we want 
+//       to get the details of student who score more than 50 marks
+for(i=0; i<=3; i++){                               
         if (studentRecords[i].marks > 50){
             console.log(studentRecords[i]); 
         }
