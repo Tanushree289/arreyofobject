3b7c906db25e00c617fb550cac55858453ff47bf
Tanushree289 <107938801+Tanushree289@users.noreply.github.com>
2022-08-26 16:59:25 +0530

    Update studentrecord.js

--- studentrecord.js
+++ studentrecord.js
@@ -1,4 +1,8 @@
-    console.log(studentRecords[0].name.toUpperCase()); 
+//let studentRecords = [{ name: 'John', id: 123, marks: 98 }, { name: 'Baba', id: 101, marks: 23 }, { name: 'yaga', id: 200, marks: 45 }, { name: 'Wick', id: 115, marks: 75 }];
+// Ques 1 : We are interested in retrieving only the name of the students and all the names should be in caps.['JOHN', 'BABA', 'YAGA', 'WICK']
+
+
+console.log(studentRecords[0].name.toUpperCase()); 
     console.log(studentRecords[1].name.toUpperCase());
     console.log(studentRecords[2].name.toUpperCase());
     console.log(studentRecords[3].name.toUpperCase());
